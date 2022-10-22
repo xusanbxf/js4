e8c6e8b9434e175a938da71105b7b7ec4424a012
xusan <xusan007>
2022-10-22 11:19:31 +0500

    js4

---
+++ main.js
@@ -0,0 +1,14 @@
+let summa = 1;
+let son = +prompt('son krting')
+let son2 = +prompt('ikkinchi son')
+for (let i = 1; i <= son; i++) {
+   summa = summa * i
+ 
+}   alert(summa);
+
+// let summa = 1;
+// let son = +prompt('son krting')
+// for (let i = 1; i <= son; i++) {
+//  summa = summa *i
+//     console.log(summa);
+// }
